585c688ef7b73fdac08d84d252d54756001aec32
Russ White <356303+russwyte@users.noreply.github.com>
2026-09-23 09:40:37 -0500

    Bump sbt to 2.1.0-M2. (#45)
    sbt-zipx 0.11.1 vendors sbt-remote-cache 2.1.0-M2, so the launcher and the plugin pin move together.

--- project/ZipxVersions.scala
+++ project/ZipxVersions.scala
@@ -9,7 +9,7 @@ import zipx.*
   * module already pulls them (specular-site via the docs theme).
   */
 object MyVersions extends ZipxVersions:
-  val sbt: SbtVersion     = SbtVersion("2.1.0-M1")
+  val sbt: SbtVersion     = SbtVersion("2.1.0-M2")
   val scala: ScalaVersion = ScalaVersion("3.9.0")
 
   val zio        = Lib("dev.zio", "zio", "2.1.26")
